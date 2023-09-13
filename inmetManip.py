d7442b477b9144a2ced3a842b4f438502e2539f6
Joao MM <71529709+JoaoMaieron@users.noreply.github.com>
2023-09-12 23:01:23 -0300

    Add files via upload

--- inmetManip.py
+++ inmetManip.py
@@ -3,7 +3,11 @@ import numpy as np
 import glob
 import os
 
-def inmetD(directory,uf):
+def inmetD(directory,uf,year):
+
+    # até GO funcionou
+    # alteracao uma linha, até MA pronto
+    # alteracao uma linha, até MG pronto
 
     csv_files = glob.glob(os.path.join(directory, "*.csv"))
 
@@ -53,44 +57,60 @@ def inmetD(directory,uf):
             new['data'].append(d)
 
             # Também precisa verificar para cada um se há pelo menos um valor válido
-            if dfDay['TEMPERATURA DO AR - BULBO SECO, HORARIA (°C)'].value_counts().get('-9999',0) == dfDay['TEMPERATURA DO AR - BULBO SECO, HORARIA (°C)'].count():
+            if dfDay['TEMPERATURA DO AR - BULBO SECO, HORARIA (°C)'].value_counts().get('-9999',0) == dfDay['TEMPERATURA DO AR - BULBO SECO, HORARIA (°C)'].count() or dfDay['TEMPERATURA DO AR - BULBO SECO, HORARIA (°C)'].value_counts().get(-9999,0) == dfDay['TEMPERATURA DO AR - BULBO SECO, HORARIA (°C)'].count():
                 new['temp_media'].append(0)
             else:
-                new['temp_media'].append(round(np.mean([float(t.replace(',','.')) for t in dfDay['TEMPERATURA DO AR - BULBO SECO, HORARIA (°C)'] if t != '-9999']),2))
+                if type(df['TEMPERATURA DO AR - BULBO SECO, HORARIA (°C)'][0]) == str: # Os do PA tava como int
+                    new['temp_media'].append(round(np.mean([float(t.replace(',','.')) for t in dfDay['TEMPERATURA DO AR - BULBO SECO, HORARIA (°C)'] if t != '-9999']),2))
+                else:
+                    new['temp_media'].append(round(np.mean([t for t in dfDay['TEMPERATURA DO AR - BULBO SECO, HORARIA (°C)'] if t != -9999]),2))
 
-            if dfDay['TEMPERATURA MÁXIMA NA HORA ANT. (AUT) (°C)'].value_counts().get('-9999',0) == dfDay['TEMPERATURA MÁXIMA NA HORA ANT. (AUT) (°C)'].count():
+            if dfDay['TEMPERATURA MÁXIMA NA HORA ANT. (AUT) (°C)'].value_counts().get('-9999',0) == dfDay['TEMPERATURA MÁXIMA NA HORA ANT. (AUT) (°C)'].count() or dfDay['TEMPERATURA MÁXIMA NA HORA ANT. (AUT) (°C)'].value_counts().get(-9999,0) == dfDay['TEMPERATURA MÁXIMA NA HORA ANT. (AUT) (°C)'].count():
                 new['temp_max'].append(0)
             else:
-                new['temp_max'].append(np.max([float(t.replace(',','.')) for t in dfDay['TEMPERATURA MÁXIMA NA HORA ANT. (AUT) (°C)'] if t != '-9999']))
-            
-            if dfDay['TEMPERATURA MÍNIMA NA HORA ANT. (AUT) (°C)'].value_counts().get('-9999',0) == dfDay['TEMPERATURA MÍNIMA NA HORA ANT. (AUT) (°C)'].count():
+                if type(df['TEMPERATURA MÁXIMA NA HORA ANT. (AUT) (°C)'][0]) == str: # Os do PA tava como int
+                    new['temp_max'].append(round(np.max([float(t.replace(',','.')) for t in dfDay['TEMPERATURA MÁXIMA NA HORA ANT. (AUT) (°C)'] if t != '-9999']),2))
+                else:
+                    new['temp_max'].append(round(np.max([t for t in dfDay['TEMPERATURA MÁXIMA NA HORA ANT. (AUT) (°C)'] if t != -9999]),2))
+
+            if dfDay['TEMPERATURA MÍNIMA NA HORA ANT. (AUT) (°C)'].value_counts().get('-9999',0) == dfDay['TEMPERATURA MÍNIMA NA HORA ANT. (AUT) (°C)'].count() or dfDay['TEMPERATURA MÍNIMA NA HORA ANT. (AUT) (°C)'].value_counts().get(-9999,0) == dfDay['TEMPERATURA MÍNIMA NA HORA ANT. (AUT) (°C)'].count():
                 new['temp_min'].append(0)
             else:
-                new['temp_min'].append(np.min([float(t.replace(',','.')) for t in dfDay['TEMPERATURA MÍNIMA NA HORA ANT. (AUT) (°C)'] if t != '-9999']))
+                if type(df['TEMPERATURA MÍNIMA NA HORA ANT. (AUT) (°C)'][0]) == str: # Os do PA tava como int
+                    new['temp_min'].append(np.min([float(t.replace(',','.')) for t in dfDay['TEMPERATURA MÍNIMA NA HORA ANT. (AUT) (°C)'] if t != '-9999']))
+                else:
+                    new['temp_min'].append(round(np.min([t for t in dfDay['TEMPERATURA MÍNIMA NA HORA ANT. (AUT) (°C)'] if t != -9999]),2))                
             
-            if dfDay['PRECIPITAÇÃO TOTAL, HORÁRIO (mm)'].value_counts().get('-9999',0) == dfDay['PRECIPITAÇÃO TOTAL, HORÁRIO (mm)'].count():
+            if dfDay['PRECIPITAÇÃO TOTAL, HORÁRIO (mm)'].value_counts().get('-9999',0) == dfDay['PRECIPITAÇÃO TOTAL, HORÁRIO (mm)'].count() or dfDay['PRECIPITAÇÃO TOTAL, HORÁRIO (mm)'].value_counts().get(-9999,0) == dfDay['PRECIPITAÇÃO TOTAL, HORÁRIO (mm)'].count():
                 new['prec_media'].append(0)
             else:
-                new['prec_media'].append(round(np.mean([float(t.replace(',','.')) for t in dfDay['PRECIPITAÇÃO TOTAL, HORÁRIO (mm)'] if t != '-9999']),2))
+                if type(df['PRECIPITAÇÃO TOTAL, HORÁRIO (mm)'][0]) == str: # Tinha UM csv do MT que não tava como string aqui, por isso esse if
+                    new['prec_media'].append(round(np.mean([float(t.replace(',','.')) for t in dfDay['PRECIPITAÇÃO TOTAL, HORÁRIO (mm)'] if t != '-9999']),2))
+                else:
+                    new['prec_media'].append(round(np.mean([t for t in dfDay['PRECIPITAÇÃO TOTAL, HORÁRIO (mm)'] if t != -9999]),2))
 
-            if dfDay['UMIDADE RELATIVA DO AR, HORARIA (%)'].value_counts().get('-9999',0) == dfDay['UMIDADE RELATIVA DO AR, HORARIA (%)'].count():
+            if dfDay['UMIDADE RELATIVA DO AR, HORARIA (%)'].value_counts().get('-9999',0) == dfDay['UMIDADE RELATIVA DO AR, HORARIA (%)'].count() or dfDay['UMIDADE RELATIVA DO AR, HORARIA (%)'].value_counts().get(-9999,0) == dfDay['UMIDADE RELATIVA DO AR, HORARIA (%)'].count():
                 new['umid_media'].append(0)
             else:
                 new['umid_media'].append(round(np.mean([t for t in dfDay['UMIDADE RELATIVA DO AR, HORARIA (%)']]),2))
 
-            if dfDay['VENTO, VELOCIDADE HORARIA (m/s)'].value_counts().get('-9999',0) == dfDay['VENTO, VELOCIDADE HORARIA (m/s)'].count():
+            if dfDay['VENTO, VELOCIDADE HORARIA (m/s)'].value_counts().get('-9999',0) == dfDay['VENTO, VELOCIDADE HORARIA (m/s)'].count() or dfDay['VENTO, VELOCIDADE HORARIA (m/s)'].value_counts().get(-9999,0) == dfDay['VENTO, VELOCIDADE HORARIA (m/s)'].count():
                 new['vento_medio'].append(0)
             else:
-                new['vento_medio'].append(round(np.mean([float(t.replace(',','.')) for t in dfDay['VENTO, VELOCIDADE HORARIA (m/s)'] if t != '-9999']),2))
+                if type(df['VENTO, VELOCIDADE HORARIA (m/s)'][0]) == str: # Os do MT tavam como int e nao string
+                    new['vento_medio'].append(round(np.mean([float(t.replace(',','.')) for t in dfDay['VENTO, VELOCIDADE HORARIA (m/s)'] if t != '-9999']),2))
+                else:
+                    new['vento_medio'].append(round(np.mean([t for t in dfDay['VENTO, VELOCIDADE HORARIA (m/s)'] if t != -9999]),2))
 
-        pd.DataFrame.to_csv(pd.DataFrame.from_dict(new), path_or_buf=directory + uf + '/' + file + '.csv')
-
+        pd.DataFrame.to_csv(pd.DataFrame.from_dict(new), path_or_buf='./' + year + '/' + uf + '/' + file.split('\\')[-1] + '.csv')
+        
 
 # Precisa remover as primeiras oito linhas dos csv, senão o pandas nao abre eles
-def removeLines():
-    ufs = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO']
+def removeLines(path):
+    ufs = ['AC','AL','AM','AP','BA','CE','DF','ES','GO','MA','MG','MS','MT','PA','PB','PE','PI','PR','RJ','RN','RO','RR','RS','SC','SE','SP','TO']
+
     for u in ufs:
-        folder = '../dados_inmet/2010/' + u + '/'
+        folder = path + u + '/'
         for filename in os.listdir(folder):
             file = os.path.join(folder,filename)
             with open(file, "r") as f:
@@ -100,11 +120,22 @@ def removeLines():
                     if number not in range(0,8):
                         f.write(line)
 
-def main():
-    ufs = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO']
+def inmetManip(path,year):
+    ufs = ['AC','AL','AM','AP','BA','CE','DF','ES','GO','MA','MG','MS','MT','PA','PB','PE','PI','PR','RJ','RN','RO','RR','RS','SC','SE','SP','TO']
     for u in ufs:
-        inmetD('../dados_inmet/2010/' + u + '/',u)
-        print(u,' Pronto')
+        inmetD(path + u + '/',u,year)
+        print(u,' pronto')
 
-if __name__ == '__main__':
-    main()
+# De 2019 em diante mudaram os nomes das colunas
+
+removeLines('../dados_inmet/2019/')
+inmetManip('../dados_inmet/2019/','2019')
+
+removeLines('../dados_inmet/2020/')
+inmetManip('../dados_inmet/2020/','2020')
+
+removeLines('../dados_inmet/2021/')
+inmetManip('../dados_inmet/2021/','2021')
+
+removeLines('../dados_inmet/2022/')
+inmetManip('../dados_inmet/2022/','2022')
