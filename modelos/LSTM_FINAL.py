7c6d5403fbe4cfc796e31b9beed99690580dfeaa
Joao MM <71529709+JoaoMaieron@users.noreply.github.com>
2023-11-29 10:29:30 -0300

    Add files via upload

--- modelos/LSTM_FINAL.py
+++ modelos/LSTM_FINAL.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 
+# Abre os csv e separa em x e y - sem os trends ainda
 def openAndSplit(uf):
     df_m = pd.read_csv('../dados_versao_final/meteorologicos/'+uf+'.csv',index_col='data',parse_dates=True)
     df_d = pd.read_csv('../dados_versao_final/doencas/'+uf+'.csv')
@@ -15,13 +16,13 @@ def openAndSplit(uf):
     
     return df_m, df_d, X, y
 
-# Transformacao MinMax
-
+# Aplica transformacao MinMax
 def scaleData(X,y,scaler):    
     X = scaler.fit_transform(X)
     y = scaler.fit_transform(y.reshape(-1, 1))
     return X, y
 
+# Monta o conjunto de treino para o formato que o LSTM aceita como entrada
 def prepareData(WS,train_size,X,y):
     # Montagem para o formato do modelo
     X = np.array([X[i-WS:i] for i in range(WS, len(X))]) # Original range(WS, len(X))
@@ -48,16 +49,16 @@ from keras.layers import Dropout
 # Camadas do modelo segundo artigo
 # https://www.tensorflow.org/api_docs/python/tf/keras/Model
 # https://www.tensorflow.org/api_docs/python/tf/keras/layers/LSTM
-
+# Aqui é onde tem que fazer ainda mais alterações - quantidade de camadas, neuronios
 def createModel(units, dropout, X_train):
     model = Sequential()
     model.add(LSTM(units=units, return_sequences = True, input_shape=(X_train.shape[1], X_train.shape[2])))
     model.add(Dropout(dropout))
-    model.add(LSTM(units = units, return_sequences = True))
+    model.add(LSTM(units = 128, return_sequences = True))
     model.add(Dropout(dropout))
-    model.add(LSTM(units = units, return_sequences = True))
+    model.add(LSTM(units = 64, return_sequences = True))
     model.add(Dropout(dropout))
-    model.add(LSTM(units = units, return_sequences = True))
+    model.add(LSTM(units = 512, return_sequences = True))
     model.add(Dropout(dropout))
     model.add(LSTM(units = units))
     model.add(Dropout(dropout))
@@ -75,54 +76,23 @@ def loadModel(path='./LSTM_final'):
 # SEM RODAR O GRIDSEARCH
 #=================================================================================
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.optimizers import RMSprop
+# from tensorflow.keras.optimizers import RMSprop
 from scikeras.wrappers import KerasRegressor
+from keras.callbacks import EarlyStopping
 
-def runModel_1(model,epochs,batch_size):
+# Compile e fit do modelo
+def runModel_1(model,epochs,batch_size,X_train,y_train,X_test):
     # optimizer=RMSprop(learning_rate=0.001)
+    early_stopping = EarlyStopping(monitor='mse', patience=50, restore_best_weights=True)
     model.compile(optimizer=Adam(learning_rate=0.001), loss = 'mean_squared_error',metrics=["accuracy", "mape", "mse"])
     
-    model.fit(X_train,y_train, epochs = epochs, batch_size = batch_size)
+    model.fit(X_train,y_train, epochs = epochs, batch_size = batch_size,callbacks=[early_stopping])
     return model, model.predict(X_test) # Se vier esquisito, usar .flatten()
 
 #=================================================================================
 # COM GRID SEARCH
 #=================================================================================
-# com learning_rate=0.0001, ws = 26
-# {'batch_size': 64, 'epochs': 200}
-# best_model:
-#     KerasRegressor(
-# 	model=None
-# 	build_fn=<keras.src.engine.sequential.Sequential object at 0x798858ed32e0>
-# 	warm_start=False
-# 	random_state=None
-# 	optimizer=rmsprop
-# 	loss=None
-# 	metrics=None
-# 	batch_size=64
-# 	validation_batch_size=None
-# 	verbose=True
-# 	callbacks=None
-# 	validation_split=0.0
-# 	shuffle=True
-# 	run_eagerly=False
-# 	epochs=200)
-
-
-# Early stopping ou patience
-# Justificar a escolha de 26 semanas
-# Hiper de poda de arvores, profundidade maxima
-# biblioteca XGBoost
-# primeiro testar os tres sem google, depois com; comparacoes a cada vez.
-# logit_grid_searcher = GridSearchCV(estimator=logit, param_grid={'C': c_values},
-#                                  scoring='roc_auc', n_jobs=1, cv=time_split, verbose=1)
-# time_split = TimeSeriesSplit(n_splits=10)
-# Trazer tabelas e graficos
-# Overleaf modelo 
-
-
 from sklearn.model_selection import GridSearchCV, TimeSeriesSplit
-from keras.callbacks import EarlyStopping
 
 #from scikeras.wrappers import KerasRegressor
 def runModelGridSearch(model, X_train):
@@ -151,44 +121,17 @@ def runModelGridSearch(model, X_train):
     grid_search.fit(X_train, y_train,callbacks=[early_stopping])
     print(grid_search.best_params_) # {'batch_size': 100, 'epochs': 300}
     best_model = grid_search.best_estimator_
-#     KerasRegressor(
-# 	model=None
-# 	build_fn=<keras.src.engine.sequential.Sequential object at 0x7f4f48adfa60>
-# 	warm_start=False
-# 	random_state=None
-# 	optimizer=rmsprop
-# 	loss=None
-# 	metrics=None
-# 	batch_size=100
-# 	validation_batch_size=None
-# 	verbose=True
-# 	callbacks=None
-# 	validation_split=0.0
-# 	shuffle=True
-# 	run_eagerly=False
-# 	epochs=300
-# )
+
     return best_model, best_model.predict(X_test)
 
 #=================================================================================
 # AVALIACAO 
 #=================================================================================
-
-# Acredito que use as metricas do grid_search
-# mse = best_model.evaluate(X_test, y_test, verbose=2)
-# mse = model.evaluate(X_test, y_test, verbose=2)
-
 from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error
 def printMetrics():
     print('R2 = ',r2_score(y_test, y_pred))
     print('MSE = ',mean_squared_error(y_test, y_pred))
-
-# from sklearn.metrics import *
-# def calculate_metrics(pred, ytrue):
-#     return [mean_absolute_error(ytrue, pred, ), explained_variance_score(ytrue, pred),
-#             mean_squared_error(ytrue, pred), mean_squared_log_error(ytrue, pred),
-#             median_absolute_error(ytrue, pred), r2_score(ytrue, pred)]
 
 #=================================================================================
 # GRAFICOS
@@ -196,7 +139,7 @@ def printMetrics():
 
 # Casos total
 import matplotlib.dates as mdates
-def fullGraph(pred=False,y_pred=[],save=False,uf = ''):
+def fullGraph(df_m,df_d,pred=False,y_pred=[],save=False,uf = ''):
     fig, ax = plt.subplots(figsize=(10,6))
     ax.xaxis.set_major_locator(mdates.AutoDateLocator())
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
@@ -234,38 +177,55 @@ def compareGraph(y_pred):
 #=================================================================================
 # EXECUCAO
 #=================================================================================
+# Rodando todos os arqs e seja o que deus quiser
+import csv
+def runAll():
+    # RR ta faltando coisa, AP nao faz predicao
+    # 14 : 'RR', 16 : 'AP',
+    UFCodes = {
+        11 : 'RO', 12 : 'AC', 13 : 'AM', 15 : 'PA',  17 : 'TO', 
+        21 : 'MA', 22 : 'PI', 23 : 'CE', 24 : 'RN', 25 : 'PB', 26 : 'PE', 27 : 'AL', 28 : 'SE', 29 : 'BA',
+        31 : 'MG', 32 : 'ES', 33 : 'RJ', 35 : 'SP', 
+        41 : 'PR', 42 : 'SC', 43 : 'RS',
+        50 : 'MS', 51 : 'MT', 52 : 'GO', 53 : 'DF'
+    }
+    WS = 26
+    train_size = 600
+    scaler = MinMaxScaler()
+    epochs = 300
+    batch_size=100
+    results = []
+    for uf in UFCodes.values():
+        df_m, df_d, X, y = openAndSplit(uf)
+        X, y = scaleData(X, y, scaler)
+        X_train, X_test, y_train, y_test = prepareData(WS, train_size, X, y)
+        model = createModel(units=64,dropout=0.2,X_train=X_train)
+        model, y_pred = runModel_1(model,epochs,batch_size,X_train,y_train,X_test)
+        r2 = r2_score(y_test, y_pred)
+        mse = mean_squared_error(y_test, y_pred)
+        y_pred = inverseTransform(y_pred,scaler)
+        y_test = inverseTransform(y_test,scaler)
+        fullGraph(df_m,df_d,pred=True,y_pred=y_pred,save=True,uf=uf)
+        results.append({'estado': uf, 'R2': r2, 'MSE': mse})
+    with open('metricas.csv', 'w',newline='') as csvfile:
+        fieldnames = ['estado','R2','MSE']
+        writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
+        writer.writeheader()
+        for r in results:
+            writer.writerow(r)
 
-# 14 : 'RR',
-UFCodes = {
-    11 : 'RO', 12 : 'AC', 13 : 'AM', 15 : 'PA', 16 : 'AP', 17 : 'TO', 
-    21 : 'MA', 22 : 'PI', 23 : 'CE', 24 : 'RN', 25 : 'PB', 26 : 'PE', 27 : 'AL', 28 : 'SE', 29 : 'BA',
-    31 : 'MG', 32 : 'ES', 33 : 'RJ', 35 : 'SP', 
-    41 : 'PR', 42 : 'SC', 43 : 'RS',
-    50 : 'MS', 51 : 'MT', 52 : 'GO', 53 : 'DF'
-}
-WS = 26
-train_size = 600
-scaler = MinMaxScaler()
-epochs = 300
-batch_size=100
-for uf in UFCodes.values():
-    df_m, df_d, X, y = openAndSplit(uf)
-    X, y = scaleData(X, y, scaler)
-    X_train, X_test, y_train, y_test = prepareData(WS, train_size, X, y)
-    model = createModel(units=64,dropout=0.2,X_train=X_train)
-    model, y_pred = runModel_1(model,epochs,batch_size)
-    y_pred = inverseTransform(y_pred,scaler)
-    y_test = inverseTransform(y_test,scaler)
-    fullGraph(pred=True,y_pred=y_pred,save=True,uf=uf)
-
+runAll()
 
 # PARA UM SÓ
-df_m, df_d, X, y = openAndSplit('MG')
+df_m, df_d, X, y = openAndSplit('DF')
 scaler = MinMaxScaler()
+
+df_d = df_d.drop(0)
+df_d.to_csv('../dados_versao_final/doencas/DF.csv',index=False)
 
 df_m = df_m.drop('2023-01-01')
 
-df_m.to_csv('../dados_versao_final/meteorologicos/RS.csv')
+df_m.to_csv('../dados_versao_final/meteorologicos/DF.csv')
 
 X, y = scaleData(X, y, scaler)
 
@@ -280,13 +240,14 @@ model = createModel(units=64,dropout=0.2,X_train=X_train)
 # saveModel(model)
 # model = loadModel()
 
-epochs = 300
+epochs = 10
 batch_size=100
-model, y_pred = runModel_1(model,epochs,batch_size)
+model, y_pred = runModel_1(model,epochs,batch_size,X_train,y_train,X_test)
 # lossGraph(model)
 model, y_pred = runModelGridSearch(model, X_train)
 
-
+r2 = r2_score(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
 
 print(y_pred.shape[0])
 
